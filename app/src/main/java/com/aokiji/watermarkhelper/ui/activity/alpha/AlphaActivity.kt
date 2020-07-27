4da2821ae2cc29330966f3f2a2676275a64a6d85
trueAndroidfans <865778865@qq.com>
2020-07-27 09:33:07 +0800

    update

--- app/src/main/java/com/aokiji/watermarkhelper/ui/activity/alpha/AlphaActivity.kt
+++ app/src/main/java/com/aokiji/watermarkhelper/ui/activity/alpha/AlphaActivity.kt
@@ -1,7 +1,11 @@
 package com.aokiji.watermarkhelper.ui.activity.alpha
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.aokiji.watermarkhelper.R
 import com.aokiji.watermarkhelper.ui.activity.main.MainActivity
@@ -13,6 +17,10 @@ class AlphaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alpha)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            setFullScreenStyle()
+        }
+
         initView()
     }
 
@@ -23,4 +31,14 @@ class AlphaActivity : AppCompatActivity() {
             finish()
         }, 3000)
     }
+
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun setFullScreenStyle() {
+        window.apply {
+            attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+    }
+
 }
